1272c2e168ba059bb2121cd0ea0505ef6e66c91a
Bahram Movlanov <bahram.movlanov@passportinc.com>
2022-07-13 20:06:04 -0600

    refractoring & custom hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-class Counter extends Component {
-  render() {
-    return (
-      <div className="Counter">
-        <p className="count">0</p>
-        <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
-        </section>
-      </div>
-    );
-  }
-}
+const storeStateInLocalStorage = (count) => {};
+
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    if (storage) return JSON.parse(storage);
+    return initialState;
+  };
+
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const Counter = ({ max, step }) => {
+  const [count, setCount] = useLocalStorage(0, 'count')
+
+  const increment = () =>
+    setCount((c) => {
+      if (c >= max) return c;
+      return c + step;
+    });
+
+  useEffect(() => {
+    document.title = `Counter is: ${count}`;
+  }, [count]);
+
+  useEffect(() => {
+    storeStateInLocalStorage(count);
+  }, [count]);
+
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
+};
 
 export default Counter;
